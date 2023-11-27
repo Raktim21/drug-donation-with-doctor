971bc089970a6ca5d68647a8fcce252b2d016dfc
Shariar Raktim <raktim21cse@gmail.com>
2023-11-27 18:47:57 +0600

    fixed

--- database/migrations/2023_11_26_104932_add_distric_id_to_drugs.php
+++ database/migrations/2023_11_26_104932_add_distric_id_to_drugs.php
@@ -12,10 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('drugs', function (Blueprint $table) {
-            $table->string('sku')->nullable()->after('id');
             $table->foreignId('district_id')->after('user_id')->nullable()->constrained('districts')->nullOnDelete();
             $table->string('address')->nullable()->after('district_id');
-            $table->tinyInteger('status')->default(0)->comment('1=approved, 0=rejected')->after('address');
         });
     }
 
